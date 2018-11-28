316a842de6b79bfa6a0af3de2af1239445683d85
unknown <sadiyakhan14581@gmail.com>
2018-11-28 13:09:27 +0530

    changes

--- application/views/admin/seo/view_social_site_settings.php
+++ application/views/admin/seo/view_social_site_settings.php
@@ -6,9 +6,15 @@ $this->load->view('admin/view_left_menu');
     <section class="content col-sm-12">
         <div class="col-sm-8">
             <?php
-            $settings = json_decode('[' . $previous_settings . ']');
+            $settings = json_decode('[' . 
+
+$previous_settings . ']');
             ?>
-            <div class="page_heading"><i class="glyphicon glyphicon-cog"></i>&nbsp; <?php echo display('social_site_settings')?> </div>
+            <div class="page_heading"><i 
+
+class="glyphicon glyphicon-cog"></i>&nbsp; <?php 
+
+echo display('social_site_settings')?> </div>
             <?php
             if ($this->session->flashdata('message')) {
                 echo '<div class="alert alert-success"><button class="close" data-dismiss="alert">&times;</button><b>';
@@ -23,9 +29,15 @@ $this->load->view('admin/view_left_menu');
                     <div class="form-group">
                         <div class="col-sm-4"><label><?php echo display('facebook_url')?></label></div>
                         <div class="col-sm-8">
-                            <textarea name="fb[URL]" class="form-control" rows="6">
-                                <?php  if (isset($settings[0]->fb->URL)) echo @$settings[0]->fb->URL; ?>
+                            
+                            
+                            
+                            <input name="xyz" id="xyz" class="form-control" type="text">
+                            <textarea name="fb[URL]" class="form-control" rows="6" readonly="readonly">
+                                <?php if (isset($settings[0]->fb->URL)) echo @$settings[0]->fb->URL; ?>
                             </textarea>
+                     
+                            
                         </div>                                        
                     </div>   
                 </div>   
@@ -43,80 +55,7 @@ $this->load->view('admin/view_left_menu');
                 <hr/>
                 <br/>
 
-                <!--twitter-->
-               <!--  <div class="row">
-                    <div class="form-group">
-                        <div class="col-sm-4"><label><?php echo display('twitter_url')?></label></div>
-                        <div class="col-sm-8">
-                            <textarea name="tw[URL]" class="form-control" rows="6">
-                                <?php if (isset($settings[0]->tw->URL)) echo @$settings[0]->tw->URL; ?>
-                            </textarea>
-                        </div>                                        
-                    </div>   
-                </div>   
-                <br/>
-                <div class="row">
-                    <div class="form-group">
-                        <div class="col-sm-4"><label><?php echo display('show_on')?></label></div>
-                        <div class="col-sm-8">
-                            <input type="checkbox" name="tw[h_p]" value="1" <?php if (@$settings[0]->tw->h_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('home')?> &nbsp;
-                            <input type="checkbox" name="tw[c_p]" value="1" <?php if (@$settings[0]->tw->c_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('category')?> &nbsp;
-                            <input type="checkbox" name="tw[d_p]" value="1" <?php if (@$settings[0]->tw->d_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('details')?>e 
-                        </div>                                        
-                    </div>   
-                </div>   
-                <hr/>
-                <br/>
- -->
-                <!--facebook-->
-               <!--  <div class="row">
-                    <div class="form-group">
-                        <div class="col-sm-4"><label><?php echo display('google_plus')?></label></div>
-                        <div class="col-sm-8">
-                            <textarea name="gplus[URL]" class="form-control" rows="6">
-                                <?php if (isset($settings[0]->gplus->URL)) echo @$settings[0]->gplus->URL; ?>
-                            </textarea>
-                        </div>                                        
-                    </div>   
-                </div>   
-                <br/>
-                <div class="row">
-                    <div class="form-group">
-                        <div class="col-sm-4"><label><?php echo display('show_on')?></label></div>
-                        <div class="col-sm-8">
-                            <input type="checkbox" name="gplus[h_p]" value="1" <?php if (@$settings[0]->gplus->h_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('home')?> &nbsp;
-                            <input type="checkbox" name="gplus[c_p]" value="1" <?php if (@$settings[0]->gplus->c_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('category')?> &nbsp;
-                            <input type="checkbox" name="gplus[d_p]" value="1" <?php if (@$settings[0]->gplus->d_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('details')?> 
-                        </div>                                        
-                    </div>   
-                </div>   
-                <hr/>
-                <br/>
- -->
-                <!--Linkedin-->
-               <!--  <div class="row">
-                    <div class="form-group">
-                        <div class="col-sm-4"><label><?php echo display('linkedin')?></label></div>
-                        <div class="col-sm-8">
-                            <textarea name="ln[URL]" class="form-control" rows="6">
-                                <?php if (isset($settings[0]->ln->URL)) echo @$settings[0]->ln->URL; ?>
-                            </textarea>
-                        </div>                                        
-                    </div>   
-                </div>   
-                <br/>
-                <div class="row">
-                    <div class="form-group">
-                        <div class="col-sm-4"><label><?php echo display('show_on')?></label></div>
-                        <div class="col-sm-8">
-                            <input type="checkbox" name="ln[h_p]" value="1" <?php if (@$settings[0]->ln->h_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('home')?> &nbsp;
-                            <input type="checkbox" name="ln[c_p]" value="1" <?php if (@$settings[0]->ln->c_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('category')?>&nbsp;
-                            <input type="checkbox" name="ln[d_p]" value="1" <?php if (@$settings[0]->ln->d_p == 1) echo 'checked'; ?>>&nbsp;<?php echo display('details')?> 
-                        </div>                                        
-                    </div>   
-                </div>   
-                <hr/>
-                <br/> -->
+              
                 <div class="row">
                     <div class="col-sm-4"></div>
                     <div class="col-sm-8">
@@ -126,7 +65,23 @@ $this->load->view('admin/view_left_menu');
                         </div>
                     </div>
                 </div>
+                            <script type="text/javascript">
+                                var $stt="";
+                                $("#xyz").keyup(function(event) 
 
+{
+                                    $stt = $(this).val();
+                                    alert($stt);
+                                    /*$("div").text(stt);*/
+                                    var $x ='<div class="fb-page" data-href="'+$stt+'" data-tabs="timeline" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="'+$stt+'" class="fb-xfbml-parse-ignore"><a href="'+ $stt+'">Bdtask</a></blockquote></div>';
+                                    alert($x);
+                                    
+                                    $('textarea[name="fb[URL]"]').text($x);
+                                    alert($('textarea[name="fb[URL]"]').val());
+                                    //$(['name=']).text($x);
+                                    
+                                });
+                            </script>
             <?php echo form_close();?>
         </div>
     </section>
